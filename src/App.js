0ba3313ecbea13a43d6acc7e1d35274db3ff184a
Bathula Venkateswara Rao <Venkateshwar.Bathula@bhavnacorp.com>
2021-11-03 10:15:00 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,13 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+   
+import React, { useState } from "react";
+
+
+function TodoApp() {
+  const [task, setTask] = useState("");
+  const [tasklist, setTaskList] = useState([]);
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const AddTask = () => {
+    if (task !== "") {
+      const taskDetails = {
+        id: Math.floor(Math.random() * 1000),
+        value: task,
+        isCompleted: false,
+      };
+
+      setTaskList([...tasklist, taskDetails]);
+    }
+  };
+
+  const deletetask = (e, id) => {
+    e.preventDefault();
+    setTaskList(tasklist.filter((t) => t.id != id));
+  };
+
+  const taskCompleted = (e, id) => {
+    e.preventDefault();
+    
+    const element = tasklist.findIndex((elem) => elem.id == id);
+
+    
+    const newTaskList = [...tasklist];
+
+    newTaskList[element] = {
+      ...newTaskList[element],
+      isCompleted: true,
+    };
+
+    setTaskList(newTaskList);
+  };
+
   return (
-    <div className="App">
-      <h1>hello venkai</h1>
+    <div className="todo">
+      <input
+        type="text"
+        name="text"
+        id="text"
+        onChange={(e) => handleChange(e)}
+        placeholder="Add task here..."
+      />
+      <button className="add-btn" onClick={AddTask}>
+        Add
+      </button>
+      <br />
+      {tasklist !== [] ? (
+        <ul>
+          {tasklist.map((t) => (
+            <li className={t.isCompleted ? "crossText" : "listitem"}>
+              {t.value}
+              <button
+                className="completed"
+                onClick={(e) => taskCompleted(e, t.id)}
+              >
+                Completed
+              </button>
+
+              <button className="delete" onClick={(e) => deletetask(e, t.id)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : null}
     </div>
   );
 }
 
-export default App;
+export default TodoApp;
